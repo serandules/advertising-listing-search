90bf10274be75990733e1a1c404d138fbe0dfdd6
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-01-19 00:01:30 +0530

    updating

---
+++ index.js
@@ -0,0 +1,20 @@
+var dust = require('dust')();
+var serand = require('serand');
+var utils = require('utils');
+var Advertisement = require('advertisement-service');
+var list = require('advertising-listing');
+
+module.exports = function (sandbox, fn, options) {
+    Advertisement.find({
+        query: options,
+        images: '288x162'
+    }, function (err, advertisements) {
+        if (err) {
+            return fn(true, serand.none);
+        }
+        list(sandbox, fn, {
+            advertisements: advertisements,
+            size: 4
+        });
+    });
+};
